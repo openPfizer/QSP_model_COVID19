18a26f05678f744776ad1fc0deb975c0d554ce01
Rohit Rao <rohit.rao@pfizer.com>
2022-01-20 09:19:25 -0500

    Update generate_figures.m
    Updated generate_figures.m. to call correct scripts

--- generate_figures.m
+++ generate_figures.m
@@ -1,12 +1,17 @@
-%% script to generate all the figures from manuscript
+% script to generate all the figures from manuscript
+
 addpath(genpath('.'))
-run potency_eua_v2.m
+run plausible_figure.m
+run BE_Blaze1Ph3.m
+run regen_cov.m
+run severity_plot.m
+run dVL_RRR_timing.m
+run supplementary_figures.m
 
 h =  findobj(allchild(0),'flat','Type','figure');
 for ii = 1:length(h)
     figno = h(ii);
     figname2 = get(figno,'Name');
-    figname2 = strcat("/covid_sequel/docs/",figname2)
     print(figno,figname2,'-dpng');
     
 end
